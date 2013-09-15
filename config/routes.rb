4e8b5fde5225ea0e3aeb0617c109df6a9d61ebb7
tkling <tyling@gmail.com>
2013-09-15 05:28:24 -0400

    that stuff should have never changed!

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ App::Application.routes.draw do
 
   resources :users
 
-  ignore '/assets/*'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
